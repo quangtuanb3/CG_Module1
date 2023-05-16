e79dcb7d0a2bc3617517cac5ee3507f01330eb39
quangtuanb3 <quangtuanb3@gmail.com>
2023-05-16 16:59:29 +0700

    Js operator exe

---
+++ Exercise/JS_Operator/operator.js
@@ -0,0 +1,11 @@
+inputWidth = prompt("Enter the width");
+inputHeight = prompt("Enter the height");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write('<br/>')
+document.write("Enter width: " + width); 
+document.write('<br/>')
+document.write("Enter height: " + height);
+document.write('<br/>')
+document.write("The area is: " + area);
