d792d8a99c79387c05f6341f2caae3b9af0aa9f9
quangtuanb3 <quangtuanb3@gmail.com>
2023-05-25 13:53:51 +0700

    reverse array

---
+++ Exercise/Array/ReverseArray/js.js
@@ -0,0 +1,48 @@
+
+function exe1() {
+    let inputArray = createArray();
+    document.write(`Input Array: </br>`)
+    displayArray(inputArray);
+    outputArray = reverseArray(inputArray);
+    document.write("</br>");
+    document.write("</br>");
+    document.write("</br>");
+    displayArray(outputArray);
+
+}
+
+function createArray() {
+    let size = 0;
+    do {
+        size = Number(prompt("Enter size: "));
+
+    } while (isNaN(size) || size < 0 || !Number.isInteger(size))
+
+    let inputArray = new Array(size);
+
+    for (let i = 0; i < inputArray.length; i++) {
+        do {
+            inputArray[i] = prompt(`Enter element ${i + 1}: `);
+
+        } while (inputArray[i] == '');
+    }
+    return inputArray;
+
+}
+
+function displayArray(arr) {
+    for (let i in arr) {
+        document.write(`${arr[i]} </br>`);
+    }
+}
+
+function reverseArray(arr) {
+    console.log(Math.floor(arr.length / 2))
+    let temp;
+    for (let i = 0; i < Math.floor(arr.length / 2); i++) {
+        temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return arr;
+}
