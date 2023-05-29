b665d07603a933ea6050bebcf746040c578540a0
quangtuanb3 <quangtuanb3@gmail.com>
2023-05-29 16:54:01 +0700

    feet and meters

---
+++ Exercise/Function/FeetAndMeters/js.js
@@ -0,0 +1,11 @@
+function changeToMeter() {
+    let foot = Number(document.getElementById("foot").value);
+    let meter = 0.305 * foot;
+    document.getElementById('meter').value = meter.toFixed(2);
+}
+
+function changeToFoot() {
+    let meter = Number(document.getElementById('meter').value);
+    let foot = meter / 0.305;
+    document.getElementById('foot').value = foot.toFixed(2);
+}
